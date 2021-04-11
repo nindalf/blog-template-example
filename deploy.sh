bc61a2f8690a7c8c99acd731774f6aa63aa0ab2b
Krishna Sundarram <krishna.sundarram@gmail.com>
2021-04-11 18:31:54 +0100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+cp blog-config.json blog-template/
+cp static/images/* blog-template/public/images
+cd blog-template
+npm install
+npm run build
---
+++ dev.sh
@@ -0,0 +1,4 @@
+cp blog-config.json blog-template/
+cp static/images/* blog-template/public/images
+cd blog-template
+npm run dev
